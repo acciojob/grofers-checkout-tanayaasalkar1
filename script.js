be67ddca6ca3d8fc8951316da91a502ba33f38c6
Tanaya Asalkar <145457627+tanayaasalkar1@users.noreply.github.com>
2026-09-16 20:24:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,10 +2,34 @@ const getSumBtn = document.createElement("button");
 getSumBtn.append("Get Total Price");
 document.body.appendChild(getSumBtn);
 
+let prices = document.querySelectorAll(".price");
+
 const getSum = () => {
-//Add your code here
-  
+
+    let total = 0;
+
+    // Calculate total
+    for (let i = 0; i < prices.length; i++) {
+        total += Number(prices[i].textContent);
+    }
+
+    // Create new row
+    let row = document.createElement("tr");
+
+    // Create first cell
+    let itemCell = document.createElement("td");
+    itemCell.textContent = "Total";
+
+    // Create second cell
+    let priceCell = document.createElement("td");
+    priceCell.textContent = total;
+
+    // Add both cells to row
+    row.appendChild(itemCell);
+    row.appendChild(priceCell);
+
+    // Add row to table
+    document.querySelector("table").appendChild(row);
 };
 
 getSumBtn.addEventListener("click", getSum);
-
